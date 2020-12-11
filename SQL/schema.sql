241ba4a33164a4ee7068ee854c1a5437eef4acfc
Tony Zhang <zhangfei@shinetechsoftware.com>
2020-12-11 16:04:19 +0800

    Update schema.sql

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA IF NOT EXISTS `smart-boss` DEFAULT CHARACTER SET utf8 ;
 
-USE `smart-boss` ;
+USE `smart-boss`;
 
 /**
 简历表
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS `smart-boss`.`Profile` (
   `Gender` CHAR(1) NOT NULL,
   `WorkYear` INT,
   `Age` INT,
-  `Expected` VARCHAR(36), --应聘岗位
+  `PositionApplied` VARCHAR(36),
   `Email` VARCHAR(200),
   `University` VARCHAR(200),
   `Phone` VARCHAR(20),
@@ -19,7 +19,6 @@ CREATE TABLE IF NOT EXISTS `smart-boss`.`Profile` (
   `Degree` VARCHAR(20),
   `Skills` TEXT,
   `ProjectExperience` TEXT,
-  `LastTimestamp` DATETIME(3) NOT NULL,
   PRIMARY KEY (`ProfileId`))
 ENGINE = InnoDB;
 
@@ -28,36 +27,36 @@ ENGINE = InnoDB;
 面试意见
 **/
 CREATE TABLE IF NOT EXISTS `smart-boss`.`Intrview`(
-    `InterviewId` VARCHAR(36),
+    `Id` VARCHAR(36),
     `ProfileId` VARCHAR(36),
     `Comments` VARCHAR(300),
     `CreateDate` DATETIME,
-    PRIMARY KEY(`InterviewId`))
+    PRIMARY KEY(`Id`))
 ENGINE = InnoDB;
 
 /**
-上传附件（非结构化文件）
+上传附件（简历-pdf excel）
 **/
 CREATE TABLE IF NOT EXISTS `smart-boss`.`Attachment`(
-    `AttachmentId` VARCHAR(36),
+    `Id` VARCHAR(36),
     `ProfileId` VARCHAR(36),
     `FileName` VARCHAR(300),
     `Alias` VARCHAR(80),
     `Suffix` VARCHAR(10),
     `Content` TEXT,
-    PRIMARY KEY(`ProfileId`))
+    PRIMARY KEY(`Id`))
 ENGINE = InnoDB;
 
 /**
 系统用户表
 **/
 CREATE TABLE IF NOT EXISTS `smart-boss`.`SysUser`(
-    `UserId` VARCHAR(36),
+    `Id` VARCHAR(36),
     `Name` VARCHAR(36),
     `NickName` VARCHAR(36),
     `Password` VARCHAR(125),
     `CreateDate` DATETIME,
-    PRIMARY KEY(`UserId`))
+    PRIMARY KEY(`Id`))
 ENGINE = InnoDB;
 
 
